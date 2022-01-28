a9014c2e54b9968642f7894c6696829be5b57f83
HADES CRONUS <adityavats122017@gmail.com>
2022-01-28 21:23:41 +0530

    Getting started on ShopPage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
-import { Route, Switch } from "react-router-dom";
+import ShopPage from "./pages/shop/shop.component";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-      </Switch>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/shop" element={<ShopPage />} />
+      </Routes>
     </div>
   );
 }
