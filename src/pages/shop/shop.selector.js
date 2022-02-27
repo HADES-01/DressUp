adbb02fce5a4faa4b620d52f1bef9a0e09a69977
Aditya Vats <adityavats122017@gmail.com>
2022-02-27 09:00:14 +0530

    New redux flow for some items

--- src/pages/shop/shop.selector.js
+++ src/pages/shop/shop.selector.js
@@ -1,24 +1,21 @@
 import { createSelector } from "reselect";
-import memoize from 'lodash.memoize';
+import memoize from "lodash.memoize";
 
-const COLLECTION_ID_MAP = {
-  hats: 1,
-  sneakers: 2,
-  jackets: 3,
-  womens: 4,
-  mens: 5,
-};
+const selectShop = (state) => state.shop;
 
-const selectShop = (state) => state.shop; 
-
-export const selectShopCollections = createSelector(
+export const selectCollections = createSelector(
   [selectShop],
   (shop) => shop.collections
 );
 
+export const selectShopCollections = createSelector(
+  [selectCollections],
+  (collections) => Object.keys(collections).map((key) => collections[key])
+);
+
 export const selectCollection = memoize((collectionUrlParam) =>
-  createSelector([selectShopCollections], (collections) =>
-    collections.find(
-      (collection) => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
-    )
-  ));
+  createSelector(
+    [selectCollections],
+    (collections) => collections[collectionUrlParam]
+  )
+);
