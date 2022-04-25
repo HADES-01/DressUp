7f5ba4f0387ee7a2b7bdad17ea83e32b5ef5e59f
Aditya Vats <adityavats122017@gmail.com>
2022-04-25 22:56:08 +0530

    Heroku Deploy

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(cors());
 
 if (process.env.NODE_ENV !== "production") require("dotenv").config();
